0b6465113a012a2261644486a40cb8cb72e7f9da
Michael Hamilton <michael.ts.hamilton@gmail.com>
2019-08-13 17:47:29 -0500

    Further style changes. Added favicon.

---
+++ index.js
@@ -0,0 +1,10 @@
+let search = document.getElementById("gSearch");
+let lucky = document.getElementById("lucky");
+
+search.addEventListener("click",()=>{
+    console.log("Searching...");
+});
+
+lucky.addEventListener("click", ()=>{
+    console.log("Lucky times.");
+});
